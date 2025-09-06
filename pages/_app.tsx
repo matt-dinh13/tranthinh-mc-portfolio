2fb9e2f0207746ae6277ee7dd6e663126547c3bb
Matt Dinh <matt.dinh13@gmail.com>
2025-09-06 19:08:55 +0700

    Fix Vercel deployment configuration for static site

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import type { AppProps } from 'next/app';
 import '../src/app/globals.css';
 import { LanguageProvider } from '../src/contexts/LanguageContext';
+import { useEffect, useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <LanguageProvider>
       <Component {...pageProps} />
